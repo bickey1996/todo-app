06d9d008e3d4ff5c2f70441678e8fb1dd610d197
bickey1996 <bickey.sah1996@gmail.com>
2020-12-12 15:48:12 +0530

    project initialised

---
+++ src/Components/Todo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div className="container"></div>;
+  }
+}
